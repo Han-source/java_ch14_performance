908ac69ecc3f0bf93ce3682aa0fe292ed093db8e
tkddnrdlz <tkddnrdlz@naver.com>
2021-04-07 08:12:30 +0900

    create

---
+++ src/pipeAndFilter/pipe_foundations/Pipe.java
@@ -0,0 +1,7 @@
+package pipeAndFilter.pipe_foundations;
+
+public interface Pipe<T> {
+    public boolean put(T obj);
+    public T nextOrNullIfEmptied() throws InterruptedException;
+    public void closeForWriting();
+}
